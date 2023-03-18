1295b66bf97b7ccf9bef1df9b7c84e63aa0be081
Victor Onibudo <tonibudo@lagetronix.com>
2023-03-18 09:01:39 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    "category": "Reaction",
+    "score": 80,
+    "icon": "./assets/images/icon-reaction.svg",
+    "color": "var(--light-red)",
+    "background": "var(--fade-light-red)",
+  },
+  {
+    "category": "Memory",
+    "score": 92,
+    "icon": "./assets/images/icon-memory.svg",
+    "color": "var(--orangey-yellow)",
+    "background": "var(--fade-orangey-yellow)",
+  },
+  {
+    "category": "Verbal",
+    "score": 61,
+    "icon": "./assets/images/icon-verbal.svg",
+    "color": "var(--green-teal)",
+    "background": "var(--fade-green-teal)",
+  },
+  {
+    "category": "Visual",
+    "score": 72,
+    "icon": "./assets/images/icon-visual.svg",
+    "color": "var(--cobalt-blue)",
+    "background": "var(--fade-cobalt-blue)",
+  }
+]
---
+++ main.js
@@ -0,0 +1,26 @@
+import jsonData from './data.js';
+
+let CARD_SUMMARY = document.querySelector(".card_summary-list")
+
+jsonData.forEach(x => {
+
+    let summary = document.createElement("div");
+    let header = document.createElement("h3")    
+    let paragraph = document.createElement("p");
+    let img = document.createElement("img")
+
+    summary.classList.add("card_sumary-body")
+    summary.style.background = x.background
+    header.innerText = x.category
+    header.style.color = x.color
+    img.src = x.icon
+    paragraph.innerHTML =`<span>${x.score}</span> / 100`
+
+    summary.append(img);
+    summary.append(header);
+    summary.append(paragraph);
+
+    CARD_SUMMARY.append(summary);
+})
+
+console.log(jsonData);
